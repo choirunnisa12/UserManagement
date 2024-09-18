df21406f82bb8cbde45bc20d3fe38ffa856a1f2b
CHOIRUNNISA <choirunnisa1821@gmail.com>
2024-09-18 20:55:26 +0700

    add validation

--- src/main/java/com/example/UserManagementt/controller/UserController.java
+++ src/main/java/com/example/UserManagementt/controller/UserController.java
@@ -5,8 +5,10 @@ import com.example.UserManagementt.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -16,7 +18,10 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody User request) {
+    public ResponseEntity<?> create(@Valid @RequestBody User request, BindingResult result) {
+        if (result.hasErrors()){
+            return ResponseEntity.badRequest().body(result.getAllErrors());
+        }
 
         User saveUser = userService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(saveUser);
--- src/main/java/com/example/UserManagementt/entity/User.java
+++ src/main/java/com/example/UserManagementt/entity/User.java
@@ -7,6 +7,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
@@ -20,7 +23,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(name = "name")
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 2, message = "Name should have 2 characters")
     private String name;
+    @Email(message = "Email should be valid")
     @Column(name = "email", unique = true)
     private String email;
     @Column(name = "birthDate")
